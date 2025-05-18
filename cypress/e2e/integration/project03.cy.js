f5614925a59eb05827092a52cb0c0b9dadd069a0
ranaabduljabbar <wardjojo_31@yahoo.com>
2025-05-18 11:05:39 -0500

    changes

--- cypress/e2e/integration/project03.cy.js
+++ cypress/e2e/integration/project03.cy.js
@@ -1,52 +1,92 @@
-describe('Booking Page Validation - TechGlobal', () => {
-    before(() => {
-      cy.visit('https://techglobal-training.com/frontend/booking');
-    });
-  
-    it('Validates radio buttons', () => {
-      cy.get('input[type="radio"][value="one-way"]')
-        .should('be.visible')
-        .and('be.enabled')
-        .and('be.checked');
-  
-      cy.get('input[type="radio"][value="round-trip"]')
-        .should('be.visible')
-        .and('be.enabled')
-        .and('not.be.checked');
-    });
-  
-    it('Validates Cabin Class label and dropdown', () => {
-      cy.contains('label', 'Cabin Class').should('be.visible');
-      cy.get('select[name="cabin-class"]').should('be.visible');
-    });
-  
-    it('Validates From and To dropdowns', () => {
-      cy.contains('label', 'From').should('be.visible');
-      cy.get('select[name="from"]').should('be.visible');
-  
-      cy.contains('label', 'To').should('be.visible');
-      cy.get('select[name="to"]').should('be.visible');
-    });
-  
-    it('Validates Depart and Return date pickers', () => {
-      cy.contains('label', 'Depart').should('be.visible');
-      cy.get('input[name="depart-date"]').should('be.visible');
-  
-      cy.contains('label', 'Return').should('be.visible');
-      cy.get('input[name="return-date"]').should('be.visible').and('be.disabled');
-    });
-  
-    it('Validates passenger count and Passenger 1 defaults', () => {
-      cy.contains('label', 'Number of passengers').should('be.visible');
-      cy.get('select[name="passenger-count"]').should('be.visible').and('have.value', '1');
-  
-      cy.contains('label', 'Passenger 1').should('be.visible');
-      cy.get('select[name="passenger-1"]').should('be.visible').and('have.value', 'Adult (16-64)');
-    });
-  
-    it('Validates BOOK button', () => {
-      cy.get('button[type="submit"]').contains('BOOK')
-        .should('be.visible')
-        .and('be.enabled');
-    });
-  });
+/// <reference types = "cypress" />
+require('cypress-plugin-steps')
+require('cypress-real-events')
+
+const bookingPage = require('../pages/bookingFormMethods.js')
+const selectors = bookingPage.selectors
+const testData = require('../fixtures/bookingFormTestData.js')
+const utils = require('../utils/utils.js')
+
+describe('TG Booking Function', () => {
+    beforeEach(() => {
+        cy.visit('https://www.techglobal-training.com/frontend/booking');
+    })
+
+    it('[TC01] Validate the default Book your trip form', () => {
+        cy.section(`Validate the trip type section - labels and input elements`)
+        bookingPage.validateTripTypeDefaults('One way')
+        bookingPage.validateTripTypeDefaults('Round trip')
+
+        cy.section(`Validate the labels and input elements`)
+        bookingPage.validateLabelsAndElements()
+
+        cy.section(`Validate the book button`)
+        bookingPage.validateBookButton()
+    })
+
+    it('[TC02] Validate the Book your trip form when Round trip is selected', () => {
+        let tripType = 'Round trip'
+        bookingPage.clickRadio(tripType)
+
+        cy.section(`Validate the labels and input elements`)
+        bookingPage.validateLabelsAndElements(tripType)
+
+        cy.section(`Validate the book button`)
+        bookingPage.validateBookButton()
+    })
+
+    it('[TC03] Validate the booking for 1 passenger and one way', () => {
+        let tripType = 'One way'
+        let cabinClass = 'Business'
+        let leavingFrom = 'IL'
+        let goingTo = 'FL'
+        let dateDepart = bookingPage.testDates().dateNextWeek
+        let dateReturn = null
+        let numPassengers = '1'
+        let passenger1Input = 'Senior (65+)'
+
+        let passengerDetails = [`Number of Passengers: ${numPassengers}`, `Passenger 1: ${passenger1Input}`]
+        bookingPage.createAssertionValuesForPassengers(passenger1Input, numPassengers, passengerDetails, cabinClass)
+
+        bookingPage.bookTrip(tripType, cabinClass, leavingFrom, goingTo, dateDepart, dateReturn)
+        bookingPage.inputPassengerDetails(numPassengers, passenger1Input)
+        bookingPage.validateSummary(tripType, leavingFrom, goingTo, dateDepart, dateReturn, passengerDetails)
+    })
+
+    it('[TC04] Validate the booking for 1 passenger and round trip', () => {
+        let tripType = 'Round trip'
+        let cabinClass = 'First'
+        let leavingFrom = 'CA'
+        let goingTo = 'IL'
+        let dateDepart = bookingPage.testDates().dateNextWeek
+        let dateReturn = bookingPage.testDates().dateNextFiveWeeks
+        let numPassengers = '1'
+        let passenger1Input = 'Adult (16-64)'
+
+        let passengerDetails = [`Number of Passengers: ${numPassengers}`, `Passenger 1: ${passenger1Input}`]
+        bookingPage.createAssertionValuesForPassengers(passenger1Input, numPassengers, passengerDetails, cabinClass)
+
+        bookingPage.bookTrip(tripType, cabinClass, leavingFrom, goingTo, dateDepart, dateReturn)
+        bookingPage.inputPassengerDetails(numPassengers, passenger1Input)
+        bookingPage.validateSummary(tripType, leavingFrom, goingTo, dateDepart, dateReturn, passengerDetails)
+    })
+
+    it('[TC05] Validate the booking for 2 passengers and one way', () => {
+        let tripType = 'One way'
+        let cabinClass = 'Premium Economy'
+        let leavingFrom = 'NY'
+        let goingTo = 'TX'
+        let dateDepart = bookingPage.testDates().dateTomorrow
+        let dateReturn = null
+        let numPassengers = '2'
+        let passenger1Input = 'Adult (16-64)'
+        let passengerDetails = [`Number of Passengers: ${numPassengers}`, `Passenger 1: ${passenger1Input}`]
+        
+        bookingPage.createAssertionValuesForPassengers(passenger1Input, numPassengers, passengerDetails, cabinClass)
+        console.log(passengerDetails)
+
+        bookingPage.bookTrip(tripType, cabinClass, leavingFrom, goingTo, dateDepart, dateReturn)
+        bookingPage.inputPassengerDetails(numPassengers, passenger1Input)
+        bookingPage.validateSummary(tripType, leavingFrom, goingTo, dateDepart, dateReturn, passengerDetails)
+    })
+})
